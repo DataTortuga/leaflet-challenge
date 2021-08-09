76310da122fb37625d36507df4a5139e02009cab
DataTortuga <dcurtis390@gmail.com>
2021-08-09 16:21:39 -0500

    adding starter code

---
+++ leaflet/Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,11 @@
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  console.log(data);
+});
+
+
+
